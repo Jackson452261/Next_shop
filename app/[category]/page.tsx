a871a03fdb948fd37c441d188889cbce75637cfa
Jackson452261 <a411758@gmail.com>
2025-01-07 21:12:25 +0800

    vercel

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 async function getData(category: string) {
   const query = `*[_type == "product" && category->name == "${category}"] {
         _id,
-          "imageUrl": images[0].asset->url,
-          price,
-          name,
-          "slug": slug.current,
-          "categoryName": category->name
+        "imageUrl": images[0].asset->url,
+        price,
+        name,
+        "slug": slug.current,
+        "categoryName": category->name
       }`;
 
   const data = await client.fetch(query);
